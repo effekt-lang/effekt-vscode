8b1526dc3d58406345526487abf1f11c57807c2a
Tim Neumann <tim.neumann@student.uni-tuebingen.de>
2020-12-31 18:49:18 +0100

    Added config support to allow switching between regular and remote debugging mode.

--- src/extension.ts
+++ src/extension.ts
@@ -1,9 +1,16 @@
 'use strict';
 
-import { ExtensionContext, workspace, window, Range, DecorationOptions } from 'vscode';
-import { LanguageClient, LanguageClientOptions, ServerOptions } from 'vscode-languageclient';
+import { ExtensionContext, workspace, window, Range, DecorationOptions, commands, languages } from 'vscode';
+import { LanguageClient, LanguageClientOptions, ServerOptions, StreamInfo, TransportKind, VersionedTextDocumentIdentifier } from 'vscode-languageclient';
 import { Monto } from './monto';
 import { platform } from 'os';
+import { inferEffect } from "./commands";
+import { print } from 'util';
+import { symlinkSync } from 'fs';
+import { execArgv } from 'process';
+
+const net = require('net');
+
 
 let client: LanguageClient;
 
@@ -27,22 +34,51 @@ export function activate(context: ExtensionContext) {
     folders.forEach(f => {
         args.push("--includes");
         args.push(f.uri.fsPath);
-    })
+    });
 
-    args.push("--server")
+    args.push("--server");
+    args.push("--debug");
 
-    let serverOptions: ServerOptions = {
-        run: {
-            command: effektCmd,
-            args: args,
-            options: {}
-        },
-        debug: {
-            command: effektCmd,
-            args: args.concat(["--debug"]),
-            options: {}
-        }
-    };
+
+    let remoteDebugMode = config.get<boolean>("remoteDebug");
+    let debugPort: any = config.get<number>("remoteDebugPort");
+
+    let serverOptions: any;
+
+    if(remoteDebugMode) {
+        console.log("Starting effekt server in remote debug mode");
+        serverOptions = () => {
+            // Connect to language server via socket
+            let socket: any = net.connect({ port: debugPort }); //defaults to 5007
+            let result: StreamInfo = {
+                writer: socket,
+                reader: socket
+            };
+            return Promise.resolve(result);
+        };
+    } else {
+        console.log("Starting effekt server in non-remote mode");
+        serverOptions = {
+            run: {
+                command: effektCmd,
+                args: args,
+                options: {}
+            },
+            debug: {
+                command: effektCmd,
+                args: args, //.concat(["--debug"]),
+                transport: { 
+                    kind: TransportKind.socket,
+                    port: debugPort,
+                },
+                options: {}
+            }
+        };
+    }
+
+    console.log("Exec Args: " + execArgv);
+    console.log("Args: " + args);
+    console.log("Effekt path: "+ effektCmd);
 
     let clientOptions: LanguageClientOptions = {
         documentSelector: [{
@@ -59,10 +95,21 @@ export function activate(context: ExtensionContext) {
         'effektLanguageServer',
         'Effekt Language Server',
         serverOptions,
-        clientOptions
+        clientOptions,
+        true
     );
 
     Monto.setup("effekt", context, client);
+
+    //decorate effect annotations
+    const effectDecoration = window.createTextEditorDecorationType({
+        opacity: '0.5',
+        borderRadius: '4pt',
+        light: { backgroundColor: "rgba(127,127,127,0.05)", color: "rgba(0,255,0,1.0)"},
+        dark: { backgroundColor: "rgba(255,255,255,0.05)", color: "rgba(0,255,0,1.0)" },
+        before: {contentText: 'Effekt:', backgroundColor: "rgba(100,100,100,1)"}
+    })
+
 
     // Decorate holes
     // ---
@@ -81,11 +128,12 @@ export function activate(context: ExtensionContext) {
     let editor = window.activeTextEditor
 
     function scheduleDecorations() {
-		if (timeout) { clearTimeout(timeout) }
+        if (timeout) { clearTimeout(timeout) }
 		timeout = setTimeout(decorate, 50);
     }
 
     const holeRegex = /<>|<{|}>/g
+    const effectRegex = /\/ *[a-zA-Z]* *=|\/ *{ *[a-zA-Z]* *} =/g
 
     /**
      * TODO clean this up -- ideally move it to the language server
@@ -97,19 +145,28 @@ export function activate(context: ExtensionContext) {
         const positionAt = editor.document.positionAt
 
         let holeDelimiters: DecorationOptions[] = []
+        let effectDelimiters: DecorationOptions[] = []
         let match;
 
-        function addDelimiter(from: number, to: number) {
+        function addDelimiter(from: number, to: number, delimiters: DecorationOptions[]) {
             const begin = positionAt(from)
             const end = positionAt(to)
-            holeDelimiters.push({ range: new Range(begin, end) })
+            delimiters.push({ range: new Range(begin, end) })
         }
 
         while (match = holeRegex.exec(text)) {
-            addDelimiter(match.index, match.index + 2)
+            addDelimiter(match.index, match.index + 2, holeDelimiters)
+        }
+
+        while (match = effectRegex.exec(text)) {
+            
+            addDelimiter(match.index, match.index +match[0].length, effectDelimiters)
         }
 
         editor.setDecorations(holeDelimiterDecoration, holeDelimiters)
+
+        editor.setDecorations(effectDecoration, effectDelimiters)
+
     }
 
     window.onDidChangeActiveTextEditor(ed => {
@@ -123,7 +180,22 @@ export function activate(context: ExtensionContext) {
 		}
     }, null, context.subscriptions);
 
-	scheduleDecorations();
+    scheduleDecorations();
+    
+
+    let commandDisposable = commands.registerCommand(
+        "extension.addConsoleLog",
+        inferEffect
+    );
+
+    //testing document selector - wil this work for .effekt files?
+    let docSelector = {
+        language: "effekt",
+        scheme: "file"
+    };
+
+
+    context.subscriptions.push(commandDisposable);
 
     context.subscriptions.push(client.start());
 }
