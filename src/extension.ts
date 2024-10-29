b9ede127258a14e13fd52217bcb29e7c9f2cdbfb
Evelyn <evelyn@fedora.fritz.box>
2024-10-29 16:01:07 +0100

    Added notebook serializer

--- src/extension.ts
+++ src/extension.ts
@@ -1,5 +1,6 @@
 'use strict';
 
+import { TextDecoder, TextEncoder } from 'util';
 import * as vscode from 'vscode';
 import { LanguageClient, LanguageClientOptions, ServerOptions, ExecuteCommandRequest, StreamInfo, ExecutableOptions } from 'vscode-languageclient';
 import { EffektManager } from './effektManager';
@@ -274,6 +275,66 @@ export async function activate(context: vscode.ExtensionContext) {
 	scheduleDecorations();
 
     context.subscriptions.push(client.start());
+
+    // Notebook
+    context.subscriptions.push(
+        vscode.workspace.registerNotebookSerializer('effekt-notebook', new SampleSerializer())
+    );
+}
+
+// Notebook
+interface RawNotebook {
+  cells: RawNotebookCell[];
+}
+
+interface RawNotebookCell {
+  source: string[];
+  cell_type: 'code' | 'markdown';
+}
+
+class SampleSerializer implements vscode.NotebookSerializer {
+  async deserializeNotebook(
+    content: Uint8Array,
+    _token: vscode.CancellationToken
+  ): Promise<vscode.NotebookData> {
+    const jsString = new TextDecoder().decode(content);
+
+    let raw: RawNotebookCell[];
+    try {
+      raw = (<RawNotebook>JSON.parse(jsString)).cells;
+    } catch {
+      raw = [];
+    }
+
+    const cells = raw.map(
+      item =>
+        new vscode.NotebookCellData(
+          item.cell_type === 'code'
+            ? vscode.NotebookCellKind.Code
+            : vscode.NotebookCellKind.Markup,
+          item.source.join('\n'),
+          item.cell_type === 'code' ? 'python' : 'markdown'
+        )
+    );
+
+    return new vscode.NotebookData(cells);
+  }
+
+  async serializeNotebook(
+    data: vscode.NotebookData,
+    _token: vscode.CancellationToken
+  ): Promise<Uint8Array> {
+    let contents: RawNotebookCell[] = [];
+
+    for (const cell of data.cells) {
+      contents.push({
+        cell_type: cell.kind === vscode.NotebookCellKind.Code ? 'code' : 'markdown',
+        source: cell.value.split(/\r?\n/g)
+      });
+    }
+
+    return new TextEncoder().encode(JSON.stringify(contents));
+  }
 }
 
 export function deactivate(): Thenable<void> | undefined {
