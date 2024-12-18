d724fca1f6b94057c4b222718073d809f39c573a
Evelyn Emelanov <evelyn.emelanov@student.uni-tuebingen.de>
2024-12-18 14:59:31 +0100

    Add notebook Controller

--- src/extension.ts
+++ src/extension.ts
@@ -1,12 +1,13 @@
 'use strict';
 
-import { TextDecoder, TextEncoder } from 'util';
 import * as vscode from 'vscode';
-import { LanguageClient, LanguageClientOptions, ServerOptions, ExecuteCommandRequest, StreamInfo, ExecutableOptions } from 'vscode-languageclient';
+import { LanguageClient, LanguageClientOptions, ServerOptions, ExecuteCommandRequest, StreamInfo, ExecutableOptions, RenameRequest } from 'vscode-languageclient';
 import { EffektManager } from './effektManager';
 import { Monto } from './monto';
 
 import * as net from 'net';
+import { NotebookSerializer } from './notebookSerializer';
+import { Controller } from './notebookController';
 
 let client: LanguageClient;
 let effektManager: EffektManager;
@@ -278,63 +279,9 @@ export async function activate(context: vscode.ExtensionContext) {
 
     // Notebook
     context.subscriptions.push(
-        vscode.workspace.registerNotebookSerializer('effekt-notebook', new SampleSerializer())
+        vscode.workspace.registerNotebookSerializer('effekt-notebook', new NotebookSerializer()),
+        new Controller(client),
     );
-}
-
-// Notebook
-interface RawNotebook {
-  cells: RawNotebookCell[];
-}
-
-interface RawNotebookCell {
-  source: string[];
-  cell_type: 'code' | 'markdown';
-}
-
-class SampleSerializer implements vscode.NotebookSerializer {
-  async deserializeNotebook(
-    content: Uint8Array,
-    _token: vscode.CancellationToken
-  ): Promise<vscode.NotebookData> {
-    const jsString = new TextDecoder().decode(content);
-
-    let raw: RawNotebookCell[];
-    try {
-      raw = (<RawNotebook>JSON.parse(jsString)).cells;
-    } catch {
-      raw = [];
-    }
-
-    const cells = raw.map(
-      item =>
-        new vscode.NotebookCellData(
-          item.cell_type === 'code'
-            ? vscode.NotebookCellKind.Code
-            : vscode.NotebookCellKind.Markup,
-          item.source.join('\n'),
-          item.cell_type === 'code' ? 'python' : 'markdown'
-        )
-    );
-
-    return new vscode.NotebookData(cells);
-  }
-
-  async serializeNotebook(
-    data: vscode.NotebookData,
-    _token: vscode.CancellationToken
-  ): Promise<Uint8Array> {
-    let contents: RawNotebookCell[] = [];
-
-    for (const cell of data.cells) {
-      contents.push({
-        cell_type: cell.kind === vscode.NotebookCellKind.Code ? 'code' : 'markdown',
-        source: cell.value.split(/\r?\n/g)
-      });
-    }
-
-    return new TextEncoder().encode(JSON.stringify(contents));
-  }
 }
 
 export function deactivate(): Thenable<void> | undefined {
