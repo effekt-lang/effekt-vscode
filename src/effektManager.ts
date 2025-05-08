cb9073e2548722a38d60a72a91cc5afbc65b356b
Jakub Schwenkbeck <Jakub@schwenkbeck.de>
2025-05-08 14:54:47 +0200

    Finalize Revert

--- src/effektManager.ts
+++ src/effektManager.ts
@@ -169,7 +169,8 @@ export class EffektManager {
                 // Try next option
             }
         }
-        throw('Effekt executable not found');
+
+        throw new Error('Effekt executable not found');
     }
 
     /**
@@ -236,25 +237,26 @@ export class EffektManager {
                 version
             };
         } catch (error) {
-                // If locateEffektExecutable fails, try to locate in global npm directory
-                const npmRoot = await this.execCommand('npm root -g');
+            // If locateEffektExecutable fails, try to locate in global npm directory
+            const npmRoot = await this.execCommand('npm root -g');
 
-                for (const execName of this.possibleEffektExecutables) {
-                    const fullPath = path.join(npmRoot, execName);
-                    if (await this.fileExists(fullPath)) {
-                        try {
-                            const version = await this.fetchEffektVersion(fullPath);
-                            return {
-                                success: true,
-                                executable: fullPath,
-                                message: `Effekt found at ${fullPath}, but not in PATH.`,
-                                version
-                            };
-                        } catch {
-                            // Executable exists but doesn't work, continue to next
-                        }
+            for (const execName of this.possibleEffektExecutables) {
+                const fullPath = path.join(npmRoot, execName);
+                if (await this.fileExists(fullPath)) {
+                    try {
+                        const version = await this.fetchEffektVersion(fullPath);
+                        return {
+                            success: true,
+                            executable: fullPath,
+                            message: `Effekt found at ${fullPath}, but not in PATH.`,
+                            version
+                        };
+                    } catch {
+                        // Executable exists but doesn't work, continue to next
                     }
                 }
+            }
+        
             return {
                 success: false,
                 message: "Effekt was installed but couldn't be located or executed. Please check your installation."
