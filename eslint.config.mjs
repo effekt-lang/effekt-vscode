7ab78759f16adb3738913c8f7e96764f50b07302
Jakub Schwenkbeck <Jakub@schwenkbeck.de>
2025-05-14 14:31:32 +0200

    implement ESLint which runs via `npx eslint` command

---
+++ eslint.config.mjs
@@ -0,0 +1,40 @@
+/**
+ * ESLint configuration for the project.
+ * 
+ * See https://eslint.style and https://typescript-eslint.io for additional linting options.
+ */
+// @ts-check
+import js from '@eslint/js';
+import tseslint from 'typescript-eslint';
+
+export default tseslint.config(
+	{
+		ignores: [
+			'**/.vscode-test',
+			'**/out',
+		]
+	},
+	js.configs.recommended,
+	...tseslint.configs.recommended,
+	...tseslint.configs.stylistic,
+	{
+		
+		rules: {
+			'curly': 'warn',
+			'@typescript-eslint/no-empty-function': 'off',
+			'@typescript-eslint/naming-convention': [
+				'warn',
+				{
+					'selector': 'import',
+					'format': ['camelCase', 'PascalCase']
+				}
+			],
+			'@typescript-eslint/no-unused-vars': [
+				'error',
+				{
+					'argsIgnorePattern': '^_'
+				}
+			]
+		}
+	}
+);
