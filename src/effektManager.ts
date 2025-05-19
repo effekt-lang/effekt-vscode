be2aefc3fc48e736d992996f9a4e25fe1fbbd3cb
Marcial Gaißert <uni@gaisseml.de>
2025-05-19 14:26:54 +0200

    Handle failure to communicate with npm gracefully

--- src/effektManager.ts
+++ src/effektManager.ts
@@ -393,10 +393,23 @@ export class EffektManager {
       const currentVersion = await this.getEffektVersion();
       const latestVersion = await this.getLatestNPMVersion(
         this.effektNPMPackage,
-      );
+      ).catch(function(err: string) {
+        return null;
+      });
 
+      if(latestVersion === null) {
+        // requesting latest version from NPM failed
+        if (currentVersion) {
+          vscode.window.showWarningMessage(
+            "Could not retrieve current Effekt version, using locally installed Effekt."
+          );
+        } else {
+          vscode.window.showErrorMessage(
+            "Effekt is not installed and we could not connect to NPM. Please check your network connection and reload."
+          );
+        }
+      } else if (
       // check if the latest version strictly newer than the current version
-      if (
         !currentVersion ||
         compareVersion(latestVersion, currentVersion, '>')
       ) {
