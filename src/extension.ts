3e82afcb397422dae2db26e0be260188d2340261
Tim Süberkrüb <dev@timsueberkrueb.io>
2025-06-06 11:43:29 +0200

    Update focused hole when new hole info is received

--- src/extension.ts
+++ src/extension.ts
@@ -281,6 +281,7 @@ function initializeHolesView(context: vscode.ExtensionContext) {
       const activeEditor = vscode.window.activeTextEditor;
       if (activeEditor && activeEditor.document.uri.toString() === params.uri) {
         holesViewProvider.updateHoles(params.holes);
+        holesViewProvider.focusHoles(activeEditor.selection.active);
       }
     },
   );
