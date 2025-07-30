c5bcaa248d29cd3afb8ccba5081714cb9cc817b1
Tim Süberkrüb <dev@timsueberkrueb.io>
2025-07-30 17:06:05 +0200

    Support server-sent syntax highlighting for hole panel bindings

--- src/holesPanel/webview/BindingItem.tsx
+++ src/holesPanel/webview/BindingItem.tsx
@@ -4,6 +4,7 @@ import {
   BindingInfo,
   fullyQualifiedName,
   TermBinding,
+  TypeBinding,
 } from '../effektHoleInfo';
 
 interface BindingItemProps {
@@ -13,13 +14,32 @@ interface BindingItemProps {
 export const BindingItem: React.FC<BindingItemProps> = ({ binding }) => {
   return (
     <div className="binding">
-      <span className="binding-term">
-        {binding.kind === BINDING_KIND_TERM
-          ? fullyQualifiedName(binding as TermBinding)
-          : binding.definition || binding.name}
-      </span>
+      <>
+        {binding.kind === BINDING_KIND_TERM ? (
+          fullyQualifiedName(binding as TermBinding)
+        ) : (binding as TypeBinding).definitionHtml ? (
+          <span
+            dangerouslySetInnerHTML={{
+              __html: (binding as TypeBinding).definitionHtml!,
+            }}
+          />
+        ) : (
+          binding.definition || binding.name
+        )}
+      </>
       {binding.kind === BINDING_KIND_TERM && (binding as TermBinding).type && (
-        <span>: {(binding as TermBinding).type}</span>
+        <>
+          {': '}
+          {(binding as TermBinding).typeHtml ? (
+            <span
+              dangerouslySetInnerHTML={{
+                __html: (binding as TermBinding).typeHtml!,
+              }}
+            />
+          ) : (
+            (binding as TermBinding).type
+          )}
+        </>
       )}
     </div>
   );
