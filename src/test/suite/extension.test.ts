a0571676147e30df0b460b2b5c2f783c7a5fbb02
Jakub Schwenkbeck <Jakub@schwenkbeck.de>
2025-08-19 12:48:39 +0200

    test setup based on the Vscode docs :https://code.visualstudio.com/api/working-with-extensions/testing-extension

---
+++ src/test/suite/extension.test.ts
@@ -0,0 +1,16 @@
+import * as assert from 'assert';
+
+import * as vscode from 'vscode';
+
+suite('Extension Test Suite', () => {
+  vscode.window.showInformationMessage('Start all tests.');
+
+  test('Sample test', () => {
+    assert.strictEqual(-1, [1, 2, 3].indexOf(5));
+    assert.strictEqual(-1, [1, 2, 3].indexOf(0));
+  });
+
+  test('Extension should be present', () => {
+    assert.ok(vscode.extensions.getExtension('effekt-lang.effekt'));
+  });
+});
