b1968a74cad695afd5a70be44dffa26a4964fcce
Jakub Schwenkbeck <Jakub@schwenkbeck.de>
2025-08-19 10:36:38 +0200

    export all hooks in index.ts file

---
+++ src/holesPanel/webview/hooks/index.ts
@@ -0,0 +1,4 @@
+export { useHolesPanelState } from './useHolesPanelState';
+export { useHoleNavigation } from './useHoleNavigation';
+export { useHoleActions } from './useHoleActions';
+export { useKeyboardNavigation } from './useKeyboardNavigation';
